68866fffe8b58a91fa813534d1d19f4735381b76
guanshouan <1311976870@qq.com>
2023-07-30 23:33:10 +0800

    支持前端路由

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const history = require("connect-history-api-fallback");
 
-const app = express()
+const app = express();
 
 // Enable All CORS Requests
 // Usage: https://www.npmjs.com/package/cors
-app.use(cors())
+app.use(cors());
 
-app.use(express.static('./dist'))
+app.use(history());
+app.use(express.static("./dist"));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get('/getData', (req, res) => {
-    res.send({
-        name: 'guan',
-        age: 18
-    })
-})
+app.get("/getData", (req, res) => {
+  res.send({
+    name: "guan",
+    age: 18,
+  });
+});
 
-app.listen('3001', err => {
-    if (!err) {
-        console.log('server running at 3001...');
-    }
-})
+app.listen("3001", (err) => {
+  if (!err) {
+    console.log("server running at 3001...");
+  }
+});
